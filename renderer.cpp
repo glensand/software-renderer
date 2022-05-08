147b910482078455786f37c2104084164c6810a0
Gleb Bezborodov <bezborodoff.gleb@gmail.com>
2022-05-08 20:16:27 +0400

    removed old implementation

--- renderer.cpp
+++ renderer.cpp
@@ -40,45 +40,7 @@ namespace renderer {
             return { (unsigned char)(255.0 * intencity), (unsigned char)(255.0 * intencity), (unsigned char)(255.0 * intencity), 255};
         }
 
-        void render_triangle(const vector3& p0_in, const vector3& p1_in, 
-            const vector3& p2_in, tga_image& in_image, const tga_image::color& color) {
-
-            if(std::abs(p0_in.y - p1_in.y) < std::numeric_limits<double>::epsilon()
-                || std::abs(p0_in.y - p2_in.y) < std::numeric_limits<double>::epsilon())
-                return;
-            
-            const auto [width, height] = in_image.size();
-
-            std::vector<vector3> tr{ p0_in, p1_in, p2_in };
-            std::sort(begin(tr), end(tr), [](const vector3& p0, const vector3& p1){
-                return p0.y < p1.y;
-            });
-
-            std::vector<point> p0_p2, p0_p1_p2;
-
-            line::rasterize({ (int)width, (int)height }, tr[0], tr[2], 
-                    [&](int x, int y) { p0_p2.emplace_back(point{ x, y }); });
-
-            line::rasterize({ (int)width, (int)height }, tr[0], tr[1], 
-                    [&](int x, int y) { p0_p1_p2.emplace_back(point{ x, y }); });
-
-            line::rasterize({ (int)width, (int)height }, tr[1], tr[2], 
-                    [&](int x, int y) { p0_p1_p2.emplace_back(point{ x, y }); });
-
-            auto&& small = p0_p2;
-            auto&& large = p0_p1_p2;
-            if(small.size() > large.size())
-                std::swap(small, large);
-
-            for(auto&& large_p : large){
-                for(auto&& small_p : small){
-                    line::brezenhaim(large_p, small_p, 
-                        [&](int x, int y) { in_image.set(x, y, color); });
-                }
-            }
-        }
-
-        void render_triangle2(point t0, point t1, point t2, 
+        void render_triangle(point t0, point t1, point t2, 
             tga_image &image, tga_image::color color) {
             if (t0.y==t1.y && t0.y==t2.y) return; // i dont care about degenerate triangles
             
@@ -126,27 +88,6 @@ namespace renderer {
         auto&& verticies = in_model.get_vert();
         auto&& triangles = in_model.get_trian();
 
-        const vector3 light_dir{ 0, 0, -1 };
-
-        for(auto&& [p0, p1, p2] : triangles) {
-            auto norm = cross(verticies[p2] - verticies[p0], 
-                verticies[p1] - verticies[p0]);
-
-            norm.normalize();
-
-            const auto intencity = norm * light_dir;
-
-            if(intencity > 0) {
-                detail::render_triangle(verticies[p0], verticies[p1], verticies[p2], in_image, 
-                    detail::by_intencity(intencity));
-            }
-        }
-    }
-
-    void rasterize2(const model& in_model, tga_image& in_image) {
-        auto&& verticies = in_model.get_vert();
-        auto&& triangles = in_model.get_trian();
-
         const auto [width, height] = in_image.size();
 
         const vector3 light_dir{ 0, 0, -1 };
@@ -165,7 +106,7 @@ namespace renderer {
                 auto&& ss_p1 = point { int((verticies[p1].x + 1.) * width / 2.), int((verticies[p1].y + 1.) * height / 2.)};
                 auto&& ss_p2 = point { int((verticies[p2].x + 1.) * width / 2.), int((verticies[p2].y + 1.) * height / 2.)};
 
-                detail::render_triangle2(ss_p0, ss_p1, ss_p2, in_image, 
+                detail::render_triangle(ss_p0, ss_p1, ss_p2, in_image, 
                     detail::by_intencity(intencity));
             }
         }
